b49dc4b72027e91e43749e110bfd1d43a8eff670
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-12-06 19:00:54 -0800

    error messages

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print("{}".format(value))
-            return True
-        else:
-            return False
+        print("{:d}".format(value))
+        return True
 
     except (ValueError, TypeError):
         print("The value is not interger")
