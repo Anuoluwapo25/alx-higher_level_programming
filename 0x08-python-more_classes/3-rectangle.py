80eded86e329a9b4768b2f5722c0809e708c366f
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-12-22 05:36:24 -0800

    rectangl

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -11,19 +11,34 @@ class Rectangle:
         self.height = height
 
     def width(self):
+        """getter
+        """
         return self._width
 
+    @width.setter
     def width(self, value):
+        """
+        setter
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self._width = value
 
+    @property
     def height(self):
+        """
+        getter
+        """
         return self._height
 
+
+    @height.setter
     def height(self, value):
+        """
+        setter
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -31,12 +46,25 @@ class Rectangle:
         self._height = value
 
     def area(self):
+        """
+        Returns:
+            area of the rectangle
+        """
         return self._width * self._height
 
     def perimeter(self):
-        return 2 * (self._width + self._height)
+        """
+        Return peremeter
+        """
+        if self._width == 0 or self._height == 0:
+            return 0
+        else:
+            return 2 * (self._width + self._height)
 
     def __str__(self):
+        """
+        returns an informal string representation of the rectangle
+        """
         if self._width == 0 or self._height == 0:
             return ""
         else:
