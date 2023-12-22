87d1e2cb5820d7f3d04dbc82bb870d2217ee0f8d
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-12-22 05:57:19 -0800

    c is fun

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+module that append
+"""
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    function append_after
+    """
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+
+        with open(filename, 'w') as file:
+            for line in lines:
+                file.write(line)
+                if search_string in line:
+                    file.write(new_string + end="")
+    except Exception as e:
+        print(f"An error occurred: {e}")
