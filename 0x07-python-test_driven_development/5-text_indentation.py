002a5cf801fc5361cb40c329ba506acb0d8c7a3d
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2023-12-30 03:31:14 -0800

    correction

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/pyhon3
+"""
+Text indentation
+"""
+
+def text_indentation(text):
+    """print text with two lines after [".", "?", ":"]
+    Args:
+        text: input
+    Raise:
+        TypeError: if text is not a string
+    module to indentify string(txt)
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    newline_chars = ['.', '?', ':']
+
+    current_line = ""
+    for char in text:
+        current_line += char
+        if char in newline_chars:
+            print(current_line.strip())
+            print("")
+            current_line = ""
+
+    if current_line:
+        print(current_line.strip())
