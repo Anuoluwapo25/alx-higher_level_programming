c0b563a39c616335c9e5fef7799c4521c1de4c61
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-01-10 16:04:58 -0800

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -80,7 +80,19 @@ class Rectangle(Base):
 
     def update(self, *args):
         """method that updates class Rectangle"""
-        if (args is None or len(args) == 0) and kwargs is not None:
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+
+        else:
             for key, value in kwargs.items():
                 if key == "id":
                     self.__id = value
@@ -92,13 +104,3 @@ class Rectangle(Base):
                     self.__x = value
                 elif key == "y":
                     self.__y == value
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
