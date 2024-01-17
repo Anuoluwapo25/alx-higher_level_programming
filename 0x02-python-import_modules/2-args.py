f005c2e9f99294b4f31ac8df6f14adcc2abbed15
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-01-16 23:46:50 -0800

    sql

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,8 @@ if (__name__ == "__main__"):
     n = len(sys.argv)
     if (n < 2):
         print("{} arguments.".format(n - 1))
-    elif (n == 2):
-        print("{} argument:".format(n - 1))
+    #elif (n == 2):
+        #print("{} argument:".format(n - 1))
     else:
         print("{} arguments:".format(n - 1))
     for i in range(1, n):
