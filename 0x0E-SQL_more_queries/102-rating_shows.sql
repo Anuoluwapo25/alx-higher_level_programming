0e7f9b7a1cb18ee6cf8303b0b33c997cf0a70ca3
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-01-18 05:59:58 -0800

    update

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,9 +1,9 @@
 -- script that lists all shows contained in hbtn_0d_tvshows
 -- without a genre linked
 
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS 'rating'
 FROM tv_shows
-LEFT JOIN tv_show_genres
-	ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.show_id IS NULL
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+INNER JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY title
+ORDER BY rating DESC;
