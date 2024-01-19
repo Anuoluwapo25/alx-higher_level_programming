f34356ac3c8f3e8fb026e91e40a66ff9a3be94d6
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-01-18 16:23:24 -0800

    max

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,6 @@
+-- Select the database
+-- Displays the max temperature of each state (ordered by State name)
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state; -- Order by state name in alphabetical order
