0496f858f93d40c97e87cb6214f870d6748793aa
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-02-14 17:21:28 -0800

    multi

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,16 +2,11 @@
 const args = process.argv.slice(2);
 let i = 0;
 const ifInt = parseInt(args[0]);
-if (!isNaN(ifInt))
-{
-	while (i < ifInt)
-	{
-		console.log('C is fun');
-		i++;
-	}
+if (!isNaN(ifInt)) {
+  while (i < ifInt) {
+    console.log('C is fun');
+    i++;
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
-else
-{
-	console.log('Missing number of occurrences');
-}
-
