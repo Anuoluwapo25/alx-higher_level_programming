b0796897a5e19551c8f1b1f9ea9846eee461dfe1
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-02-24 03:23:30 -0800

    corrections

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,22 @@
+#!/usr/bin/env python3
+
 import MySQLdb
 import sys
 
-if __name__ = "__main__":
-    username, password, database = argv[1], argv[2], argv[3]
+if __name__ == "__main__":
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
     mydata = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
 
     curobj = mydata.cursor()
 
-    query = "SELECT * FROM states ORDER BY id"
+    query = "SELECT * FROM states ORDER BY 'id' ASC"
     curobj.execute(query)
 
     states = curobj.fetchall()
 
-    for state in states:
-        print(state)
+    for rows in states:
+        print(rows)
 
         curobj.close()
         mydata.close()
