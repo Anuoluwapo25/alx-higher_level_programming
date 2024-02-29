59649e2182fed4863141ce9b98f4559a9bf0e208
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-02-29 07:58:57 -0800

    adv task

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// a script that imports an array and coputes a new array
+const list = require('./100-data').list;
+
+const newList = list.map((x, index) => x * index);
+console.log(list);
+console.log(newList);
