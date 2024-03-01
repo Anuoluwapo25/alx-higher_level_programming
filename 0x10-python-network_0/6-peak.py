482429efea37bda3baf2c0b9bbe5e12340f7e597
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-02-29 17:03:33 -0800

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
+"""Find a Peak in a List"""
 def find_peak(list_of_integers):
-    """
-    Find a peak in a list of unsorted integers.
+    """Find a Peak in a list
 
     Args:
-    - list_of_integers: List of unsorted integers
-
+        list_of_integers: list of int
     Returns:
-    - A peak element
+        the value or None
     """
-    if list_of_integers is None or len(list_of_integers) == 0:
-        return None
-
-    left, right = 0, len(list_of_integers) - 1
-    while left < right:
-        mid = (left + right) // 2
-        if list_of_integers[mid] > list_of_integers[mid + 1] and \
-                list_of_integers[mid] > list_of_integers[mid - 1]:
-            return list_of_integers[mid]
-        elif list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return list_of_integers[left]
+    lis = list_of_integers
+    b = len(lis)
+    if (b <= 2):
+        return (None)
+    for i in range(1, b - 1):
+	    if (lis[i - 1] <= lis[i] >= lis[i + 1]):
+	        return (lis[i])
+    return (None)
