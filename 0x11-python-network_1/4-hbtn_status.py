0fe953e14eb88c8b4225f0c4d0305938dc58f19c
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-03-01 16:45:51 -0800

    traceback error

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,13 +4,13 @@
 Python script that fetches https://alx-intranet.hbtn.io/status
 """
 
-import request
+import requests
 
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
-    httpResponse = requests.get(url)
+    httpResp = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(httpResponse.text)))
-    print("\t- content: {}".format(httpResponse.text))
+    print("\t- type: {}".format(type(httpResp.text)))
+    print("\t- content: {}".format(httpResp.text))
 
 
